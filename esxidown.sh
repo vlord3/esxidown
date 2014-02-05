25e56baf3ce6a08f99dce6e31659b986ab86257d
vlord3 <vlord3@gmail.com>
2014-02-05 15:52:22 -0500

    Update esxidown.sh
    This works slightly differently in that it is used to quickly down all your vm's gracefully and then place your system into maintenance mode. Useful for patching.

--- esxidown.sh
+++ esxidown.sh
@@ -1,87 +1,53 @@
 #!/bin/sh
-# ESXi 5.1 host automated shutdown script
+# ESXi 5.1 host automated vm shutdown - enter maintenance mode script
 
 # these are the VM IDs to shutdown in the order specified
 # use the SSH shell, run "vim-cmd vmsvc/getallvms" to get ID numbers
 # specify IDs separated by a space
 SERVERIDS=$(vim-cmd vmsvc/getallvms | sed -e '1d' -e 's/ \[.*$//' | awk '$1 ~ /^[0-9]+$/ {print $1}')
 
-# New variable to allow script testing, assuming the vim commands all work to issue shutdowns
-# can be "0" or "1"
-TEST=0
-
-# script waits WAIT_TRYS times, WAIT_TIME seconds each time
-# number of times to wait for a VM to shutdown cleanly before forcing power off.
-WAIT_TRYS=20
-
-# how long to wait in seconds each time for a VM to shutdown.
-WAIT_TIME=10
-
-# ------ DON'T CHANGE BELOW THIS LINE ------
-
-validate_shutdown()
-{
+# read each line as a server ID and shutdown/poweroff
+for SRVID in $SERVERIDS
+do
     vim-cmd vmsvc/power.getstate $SRVID | grep -i "off" > /dev/null 2<&1
     STATUS=$?
 
     if [ $STATUS -ne 0 ]; then
-        if [ $TRY -lt $WAIT_TRYS ]; then
-            # if the vm is not off, wait for it to shut down
-            TRY=$((TRY + 1))
-            echo "Waiting for guest VM ID $SRVID to shutdown (attempt #$TRY)..."
-            sleep $WAIT_TIME
-            validate_shutdown
-        else
-            # force power off and wait a little (you could use vmsvc/power.suspend here instead)
-            echo "Unable to gracefully shutdown guest VM ID $SRVID... forcing power off."
-            if [ $TEST -eq 0 ]; then
-                vim-cmd vmsvc/power.off $SRVID
-            fi
-            sleep $WAIT_TIME
-        fi
-    fi
-}
-
-# enter maintenance mode immediately
-echo "Entering maintenance mode..."
-if [ $TEST -eq 0 ]; then
-    esxcli system maintenanceMode set -e true -t 0 &
-fi
-
-# read each line as a server ID and shutdown/poweroff
-for SRVID in $SERVERIDS
-do
-    TRY=0
-
-    vim-cmd vmsvc/power.getstate $SRVID | grep -i "off\|Suspended" > /dev/null 2<&1
-    STATUS=$?
-
-    if [ $STATUS -ne 0 ]; then
         echo "Attempting shutdown of guest VM ID $SRVID..."
-        if [ $TEST -eq 0 ]; then
-            vim-cmd vmsvc/power.shutdown $SRVID
-        fi
-        validate_shutdown
+        vim-cmd vmsvc/power.shutdown $SRVID
     else
         echo "Guest VM ID $SRVID already off..."
     fi
 done
 
 # guest vm shutdown complete
-echo "Guest VM shutdown complete..."
 
-# shutdown the ESXi host
-echo "Shutting down ESXi host after 10 seconds..."
-if [ $TEST -eq 0 ]; then
-    esxcli system shutdown poweroff -d 10 -r "Automated ESXi host shutdown - esxidown.sh"
-fi
+for SRVID in $SERVERIDS
+do
+	vim-cmd vmsvc/power.getstate $SRVID | grep -i "off" > /dev/null 2<&1
+    STATUS=$?
+	if [ $STATUS -ne 0 ]; then
+		while [ $STATUS -ne 0 ]; do
+			echo "Waiting for VM ID $SRVID to shutdown."
+			sleep 5
+			vim-cmd vmsvc/power.getstate $SRVID | grep -i "off" > /dev/null 2<&1
+      	  STATUS=$?
+		done
+		echo "Guest VM $SRVID shutdown complete."
+	fi
+done
 
-# exit maintenance mode immediately before server has a chance to shutdown/power off
-# NOTE: it is possible for this to fail, leaving the server in maintenance mode on reboot!
-echo "Exiting maintenance mode..."
-if [ $TEST -eq 0 ]; then
-    esxcli system maintenanceMode set -e false -t 0
-fi
+# enter maintenance mode immediately
 
+esxcli system maintenanceMode get | grep -i "Disabled" > /dev/null 2<&1
+STATUS=$?
+    if [ $STATUS -eq 0 ]; then
+		echo "Entering maintenance mode..."
+		esxcli system maintenanceMode set -e true -t 30 &
+	else
+		echo "System is already in maintenance mode."
+	fi
+			
 # exit the session
+echo "Done!"
 exit
